4701cb70a717d5f3c198d0ed174260c0e6ebf9bf
Siddhartha Reddy Machi <siddhartha.machi@gmail.com>
2024-05-20 15:18:36 +0530

    Added views to applications.

--- rentifyAuth/views.py
+++ rentifyAuth/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework import parsers, renderers
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from .serializers import *
+from .models import *
+
+# Rentify user API views
+
+
+class UserRegisterAPI(CreateAPIView):
+    parser_classes = (
+        parsers.FormParser,
+        parsers.MultiPartParser,
+        parsers.JSONParser,
+    )
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = RegisterUserSerializer
+    permission_classes = []
+
+
+class UserListAPI(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        role = self.request.query_params.get("role")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        if role:
+            queryset = queryset.filter(role=role)
+        if first_name:
+            queryset = queryset.filter(first_name=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name=last_name)
+        return queryset
+
+
+class UserRetrieveAPI(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "email"
+    permission_classes = [IsAuthenticated]
--- rentifyV1/views.py
+++ rentifyV1/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+class PropertyAPIVIEW(viewsets.ModelViewSet):
+    serializer_class = PropertyListSerializer
+    search_fields = ["place", "area"]
+    ordering = ["price"]
+
+    def get_queryset(self):
+        queryset = Property.objects.all()
+        b_rooms = self.request.query_params.get("bedrooms")
+        baths = self.request.query_params.get("baths")
+        price = self.request.query_params.get("price")
+
+        if b_rooms:
+            queryset = queryset.filter(bedrooms=b_rooms)
+        if baths:
+            queryset = queryset.filter(baths=baths)
+        if price:
+            queryset = queryset.filter(price=price)
+        return queryset
+
+
+class TagAPIVIEW(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class CategoryAPIVIEW(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
