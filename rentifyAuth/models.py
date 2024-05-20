e11385ee51d156e25d46e5bbba7218c883891441
Siddhartha Reddy Machi <siddhartha.machi@gmail.com>
2024-05-20 14:47:16 +0530

    Add models definitions in rentify auth and v1.

--- rentifyAuth/models.py
+++ rentifyAuth/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import UserManager, AbstractUser
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from rentify.settings import AUTH_USER_MODEL
+
+
+class RentifyUserManager(UserManager):
+    def _create_user(self, email, password, **extra_args):
+        if not email:
+            raise ValueError("Email is required to create a user")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_args)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, role=None, password=None, **extra_args):
+        extra_args.setdefault("is_staff", False)
+        extra_args.setdefault("is_superuser", False)
+        if not role or role not in ["buyer", "seller"]:
+            role = "buyer"
+
+        extra_args.setdefault("role", role)
+
+        return self._create_user(email, password, **extra_args)
+
+    def create_superuser(self, email, password=None, **extra_args):
+        extra_args.setdefault("is_staff", True)
+        extra_args.setdefault("is_superuser", True)
+        extra_args.setdefault("role", "admin")
+
+        if extra_args.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_args.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_args)
+
+
+class User(AbstractUser):
+
+    username = None
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=6,
+        choices=(
+            ("admin", "Adminstrator"),
+            ("buyer", "Buyer"),
+            ("seller", "Seller"),
+        ),
+        default="buyer",
+    )
+    contact = models.CharField(max_length=13)
+
+    objects = RentifyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["contact"]
+
+    class Meta:
+        unique_together = ("first_name", "last_name")
+
+    def __str__(self):
+        return self.email
+
+    def clean(self):
+        if not self.is_superuser and self.role == "admin":
+            raise ValidationError(
+                {"role": _("Selected role cannot be assigned to the user.")}
+            )
--- rentifyV1/models.py
+++ rentifyV1/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from rentify.settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name="Property image", upload_to="property_pics")
+
+    def __str__(self):
+        return "Image" + str(self.id)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Tag name")
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Category name")
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Location name")
+
+    def __str__(self):
+        return self.name
+
+
+class Property(models.Model):
+    """
+    Defines the property model definition
+    with all the essential attributes.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Property name", unique=True)
+    price = models.BigIntegerField(verbose_name="Price of the property")
+    location = models.OneToOneField(to=Location, on_delete=models.CASCADE)
+    place = models.CharField(max_length=250, verbose_name="Where property is located")
+    street = models.CharField(
+        max_length=150, verbose_name="Street in which proerty located"
+    )
+    bedrooms = models.PositiveIntegerField(
+        verbose_name="No of bed rooms in the property"
+    )
+    baths = models.PositiveIntegerField(verbose_name="No of baths in the property")
+
+    seller = models.OneToOneField(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="prop_seller"
+    )
+
+    like = models.ManyToManyField(AUTH_USER_MODEL, blank=True, related_name="likes")
+    wishlist = models.ManyToManyField(
+        AUTH_USER_MODEL, blank=True, related_name="whishlist"
+    )
+    images = models.OneToOneField(to=Image, blank=True, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(to=Tag, blank=True)
+    category = models.ManyToManyField(to=Category, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if self.seller.role != "seller":
+            raise ValidationError(
+                {
+                    "seller": _(
+                        "Invalid role assigned to the field. Only user with seller role can be assigned to the field."
+                    )
+                }
+            )
