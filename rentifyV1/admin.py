d47a5edd241dfa282c57d9ba0107f91f618e9c3e
Siddhartha Reddy Machi <siddhartha.machi@gmail.com>
2024-05-20 14:52:30 +0530

    Models registration to admin site.

--- rentifyV1/admin.py
+++ rentifyV1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+# registering the models to the admin site
+admin.site.register(Property)
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Category)
